47dafea91d4b779cd28ca63fe45904228b998220
suncheng <sunchenggx@163.com>
2016-08-18 17:12:05 +0800

    baseactivity

---
+++ app/src/main/java/com/suncheng/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.suncheng.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.suncheng.myapplication.framework.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onActivityCreate(Bundle savedInstanceState) {
+        setTitle("演示");
+        setContentView(R.layout.activity_main);
+        setBackListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
