503362ce96301bf05157280c8115f67bfe039312
suncheng <sunchenggx@163.com>
2016-08-23 16:48:48 +0800

    提交catch

--- app/src/main/java/com/suncheng/myapplication/MainActivity.java
+++ app/src/main/java/com/suncheng/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.suncheng.myapplication;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import com.suncheng.myapplication.framework.BaseActivity;
 
@@ -28,6 +30,22 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
+    }
+
+    private long exitTime = 0;
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
+            if((System.currentTimeMillis() - exitTime) > 2000){
+                Toast.makeText(getApplicationContext(), "再按一次退出程序", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                finish();
+                System.exit(0);
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
 }
