5a8b01140903e27c964049c91eb851f3862337d2
suncheng <616203426@qq.com>
2016-08-23 14:25:54 +0800

    crash

---
+++ crashcatch/src/main/java/com/suncheng/crashcatch/CrashHandler.java
@@ -0,0 +1,186 @@
+package com.suncheng.crashcatch;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Looper;
+import android.widget.Toast;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.lang.reflect.Field;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import de.mindpipe.android.logging.log4j.LogConfigurator;
+
+/**
+ * Created by suncheng on 2016/8/23.
+ */
+public class CrashHandler implements Thread.UncaughtExceptionHandler {
+
+    // 系统默认的UncaughtException处理类
+    private Thread.UncaughtExceptionHandler mDefaultHandler;
+    // CrashHandler实例
+    private static CrashHandler instance;
+    // 程序的Context对象
+    private Context mContext;
+    // 用来存储设备信息和异常信息
+    private Map<String, String> infos = new HashMap<String, String>();
+
+    private String mPackageName;
+
+    /** 保证只有一个CrashHandler实例 */
+    private CrashHandler() {
+    }
+
+    /** 获取CrashHandler实例 ,单例模式 */
+    public static CrashHandler getInstance() {
+        if (instance == null)
+            instance = new CrashHandler();
+        return instance;
+    }
+
+    /**
+     * 初始化
+     */
+    public void init(Context context, String packname) {
+        mContext = context;
+        mPackageName = packname;
+        // 获取系统默认的UncaughtException处理器
+        mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+        // 设置该CrashHandler为程序的默认处理器
+        Thread.setDefaultUncaughtExceptionHandler(this);
+    }
+
+    /**
+     * 当UncaughtException发生时会转入该函数来处理
+     */
+    @Override
+    public void uncaughtException(Thread thread, Throwable ex) {
+        if (!handleException(ex) && mDefaultHandler != null) {
+            // 如果用户没有处理则让系统默认的异常处理器来处理
+            mDefaultHandler.uncaughtException(thread, ex);
+        } else {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                //logger.e(e.toString());
+            }
+            // 退出程序
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+        }
+    }
+
+    /**
+     * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成.
+     *
+     * @param ex
+     * @return true:如果处理了该异常信息;否则返回false.
+     */
+    private boolean handleException(Throwable ex) {
+        if (ex == null) {
+            return false;
+        }
+        // 收集设备参数信息
+        collectDeviceInfo(mContext);
+        // 使用Toast来显示异常信息
+        new Thread() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                Toast.makeText(mContext, "程序出现异常,即将退出", Toast.LENGTH_SHORT)
+                        .show();
+                Looper.loop();
+            }
+        }.start();
+        // 保存日志文件
+        saveCatchInfoFile(ex);
+        return true;
+    }
+
+    /**
+     * 收集设备参数信息
+     */
+    private void collectDeviceInfo(Context ctx) {
+        // 获取当前程序的版本号. 版本的id
+        try {
+            PackageManager pm = ctx.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(ctx.getPackageName(),
+                    PackageManager.GET_ACTIVITIES);
+            if (pi != null) {
+                String versionName = pi.versionName == null ? "null"
+                        : pi.versionName;
+                String versionCode = pi.versionCode + "";
+                infos.put("versionName", versionName);
+                infos.put("versionCode", versionCode);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            //logger.e(e.toString());
+        }
+        // 获取手机的硬件信息
+        // 通过反射获取系统的硬件信息
+        Field[] fields = Build.class.getDeclaredFields();
+        for (Field field : fields) {
+            try {
+                field.setAccessible(true);
+                infos.put(field.getName(), field.get(null).toString());
+            } catch (Exception e) {
+                //logger.e(e.toString());
+            }
+        }
+    }
+
+    /**
+     * 保存日志文件
+     *
+     * @param ex
+     */
+    private void saveCatchInfoFile(Throwable ex) {
+
+        StringBuffer sb = new StringBuffer();
+        for (Map.Entry<String, String> entry : infos.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            sb.append(key + "=" + value + "\n");
+        }
+
+        Writer writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        ex.printStackTrace(printWriter);
+        Throwable cause = ex.getCause();
+        while (cause != null) {
+            cause.printStackTrace(printWriter);
+            cause = cause.getCause();
+        }
+        printWriter.close();
+        String result = writer.toString();
+        sb.append(result);
+
+        saveLog(sb.toString());
+    }
+
+    private void saveLog(String sb) {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+        String time = df.format(new Date());
+        final LogConfigurator logConfigurator = new LogConfigurator();
+        logConfigurator.setFileName(Environment.getExternalStorageDirectory() + File.separator + mPackageName + "/" + time + ".log");
+        // Set the root log level
+        logConfigurator.setRootLevel(Level.DEBUG);
+        // Set log level of a specific logger
+        logConfigurator.setLevel("org.apache", Level.ERROR);
+        logConfigurator.configure();
+        Logger gLogger = Logger.getLogger("crash");
+        gLogger.debug(sb);
+    }
+}
